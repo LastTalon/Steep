1919760a0b696bc3365cc212c457aad862a3811a
Lucas Gangstad <last_talon@new.rr.com>
2017-11-15 15:51:08 -0600

    Attempt to solve travis ci sublime hanging issue

--- tests/test_application_commands.py
+++ tests/test_application_commands.py
@@ -4,8 +4,15 @@ from Steep import steep
 
 class TestApplicationCommands(unittest.TestCase):
 	def setUp(self):
+		self.view = sublime.active_window().new_file()
 		settings = sublime.load_settings("Preferences.sublime-settings")
 		settings.set("close_windows_when_empty", False)
+	
+	def tearDown(self):
+		if self.view:
+			self.view.set_scratch(True)
+			self.view.window().focus_view(self.view)
+			self.view.window().run_command("close_file")
 	
 	def test_connect(self):
 		dummy = steep.SteepConnectCommand()
